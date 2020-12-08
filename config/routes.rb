f182585b7cdbee0e519bbc38ec8c1d4783fbccfc
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-08 13:23:16 +0900

    トップページ表示用仮ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+  resources :homes, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
