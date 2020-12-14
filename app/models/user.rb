694b2bf8f159281834fd99661e8fe3cf9908f567
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-15 00:18:21 +0900

    バリデーションの追加(動作未確認)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, format: { with: /\A.+?@.+\z/ }
+  users_column = [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday]
+  users_column.each do |column|
+    validates column, presense: true
+  end
+
+  validates :email, uniqueness: true, format: { with: /\A.+?@.+\z/ }
+  validates :encrypted_password, length: { minimum: 6 } ,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ン]+\z/ }
+  validates :birthday
+
 end
