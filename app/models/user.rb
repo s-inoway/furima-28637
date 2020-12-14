9b3e15d490849e70146bde6a8b7454d52e01d7cc
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-14 23:15:37 +0900

    メールに「@」を含むバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: { with: /\A.+?@.+\z/ }
 end
