68028df8c92265e7ccd83952b4a2193e4318a098
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-15 22:27:57 +0900

    presenceバリデーションをひとまとめにし、deviseの初めから入っているバリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-         validates :email, presense: true, uniqueness: true, format: { with: /\A.+?@.+\z/ }
-         validates :encrypted_password, presense: true, length: { minimum: 6 } ,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
-         validates :first_name, :last_name, presense: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-         validates :first_name_furigana, :last_name_furigana, presense: true, format: { with: /\A[ァ-ン]+\z/ }
-         validates :birthday, presense: true
-
+  validates_presence_of :email, :encrypted_password, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday
+  validates :email, uniqueness: true, format: { with: /\A.+?@.+\z/ }
+  validates :encrypted_password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ン]+\z/ }
 end
