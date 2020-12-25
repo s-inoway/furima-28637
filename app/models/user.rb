f785e521f567b9387953f6b81aa48fd49fd17792
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-25 23:06:35 +0900

    モデル内、encrypted_passwordをpasswordに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  validates_presence_of :email, :encrypted_password, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday
+  validates_presence_of :email, :password, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday
   validates :email, uniqueness: true, format: { with: /\A.+?@.+\z/ }
-  validates :encrypted_password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ン]+\z/ }
 end
