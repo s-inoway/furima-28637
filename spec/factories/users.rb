6ba2f78e05b33af299e75749d26a7af73f7a4fe1
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-25 23:08:22 +0900

    factorybotの中身を少し変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname            {Faker::Name.name}
-    email               {Faker::Internet.free_email}
-    encrypted_password  {Faker::Internet.password(min_length: 6)}
-    first_name          {Faker::Japanese::Name.first_name}
-    last_name           {Faker::Japanese::Name.last_name}
-    first_name_furigana {Faker::Japanese::Name.first_name.yomi}
-    last_name_furigana  {Faker::Japanese::Name.last_name.yomi}
-    birthday            {Faker::Date.birthday}
+    nickname              {Faker::Games::Pokemon.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {Faker::Japanese::Name.first_name}
+    last_name             {Faker::Japanese::Name.last_name}
+    first_name_furigana   {Faker::Japanese::Name.first_name.yomi}
+    last_name_furigana    {Faker::Japanese::Name.last_name.yomi}
+    birthday              {Faker::Date.birthday}
   end
 end
