b22a07ffe0f93576c3c7baca0377e515c286501d
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-31 14:24:19 +0900

    usersモデルのバリデーション微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
   validates_presence_of :email, :password, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday
-  validates :email, uniqueness: true, format: { with: /\A.+?@.+\z/ }
+  validates :email, uniqueness: {case_sensitive: true}, format: { with: /\A.+?@.+\z/ }
   validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ン]+\z/ }
