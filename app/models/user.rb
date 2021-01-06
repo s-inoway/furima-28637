27355dff18247d13bdc7f682704ed5b2665676c3
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-01-07 00:06:24 +0900

    パスワードのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: true, format: { with: /\A.+?@.+\z/ }
-    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :password_confirmation
     validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
     validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
