f297589064d43cdc2be368213fc1e09cb48fe7f9
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-01-15 10:51:54 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_105647) do
+ActiveRecord::Schema.define(version: 2021_01_14_142456) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "carriage_id", null: false
+    t.integer "pref_id", null: false
+    t.integer "ship_date_id", null: false
+    t.integer "price", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_01_14_105647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
