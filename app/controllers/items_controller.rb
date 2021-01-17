85b62bf10daa30ddde1f5d99d8f550972409822a
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-01-17 23:25:50 +0900

    コントローラーでparamsを受け取るよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,17 @@
 class ItemsController < ApplicationController
 
+  def index
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :user_id, :name, :comment, :category_id, :condition_id, :carriage_id, :pref_id, :ship_date_id, :price
+    )
+  end
 end
