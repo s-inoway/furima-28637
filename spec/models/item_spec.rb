945d1a356e184494b14b623aa67b5a5a43184cbd
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-01-21 00:04:46 +0900

    rubocopの使用

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Image can't be blank"
       end
-      
+
       it '商品名が無いと出品できない' do
         @item.name = ''
         @item.valid?
@@ -88,55 +88,55 @@ RSpec.describe Item, type: :model do
       it '商品のカテゴリーが(---)だと出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Category must be other than 1"
+        expect(@item.errors.full_messages).to include 'Category must be other than 1'
       end
 
       it '商品の状態が(---)だと出品できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Condition must be other than 1"
+        expect(@item.errors.full_messages).to include 'Condition must be other than 1'
       end
 
       it '配送料の負担が(---)だと出品できない' do
         @item.carriage_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Carriage must be other than 1"
+        expect(@item.errors.full_messages).to include 'Carriage must be other than 1'
       end
 
       it '発送元の地域が(---)だと出品できない' do
         @item.pref_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Pref must be other than 1"
+        expect(@item.errors.full_messages).to include 'Pref must be other than 1'
       end
 
       it '発送までの日数が(---)だと出品できない' do
         @item.ship_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Ship date must be other than 1"
+        expect(@item.errors.full_messages).to include 'Ship date must be other than 1'
       end
-      
+
       it '価格が無いと出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
-      
+
       it '価格が300円より小さいと出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
       end
-      
+
       it '価格が10,000,000円以上だと出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price must be less than 10000000"
+        expect(@item.errors.full_messages).to include 'Price must be less than 10000000'
       end
-      
+
       it '価格が全角数字だと出品できない' do
         @item.price = '１００００'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
     end
   end
