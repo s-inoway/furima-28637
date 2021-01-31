d0ed2fd97daf9229e0847c3c65b4a223bb681485
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-01-31 15:20:38 +0900

    削除できなかった場合、削除しないよう条件分岐を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
-      @item.destroy
+    redirect_to item_path unless current_user.id == @item.user_id
+    if @item.destroy
       redirect_to root_path
     else
       render 'show'
