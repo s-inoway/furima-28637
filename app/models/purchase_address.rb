33a18eba5511a70eda3498276e50f2ed0563b7ba
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-02-02 21:24:31 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :purchase_id, :zip_code, :pref_id, :city, :chome, :bldg, :tel
+
+  with_options presence: true do
+    validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :pref_id,  numericality: { other_than: 1 }
+    validates :city
+    validates :chome
+    validates :tel,      format: { with: /\A\d{11}\z/ }
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(purchase_id: purchase.id, zip_code: zip_code, pref_id: pref_id, city: city, chome: chome, bldg: bldg, tel: tel)
+  end
+
+
+
+end
